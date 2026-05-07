725817c1561b9487aa2b6a2535a2e0eff5b516d4
mabrennan-bit <mabrennan@ctemc.org>
2026-05-07 11:17:35 -0400

    5/7/26

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene("res://Scenes/Intro/intro_video.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene("res://Scenes/Main/Credits.tscn")
+
+
+func _on_manual_pressed() -> void:
+	pass # Replace with function body.
