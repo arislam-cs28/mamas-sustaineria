1bc0a9f4e8cad45e7dc922cb2fd945516161188d
blueheart.elf <arislam.cs28@gmail.com>
2026-05-11 19:16:16 -0400

    Added all placeholder characters

--- Characters/chara.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
