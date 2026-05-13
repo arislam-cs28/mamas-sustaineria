250cf1fd06067ba174f73d4d516d85ba81a9796c
blueheart.elf <arislam.cs28@gmail.com>
2026-05-13 00:11:52 -0400

    moved around sprites, did not change around code

--- Scripts/ordering.gd
+++ Scripts/ordering.gd
@@ -7,14 +7,14 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 
 func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
 	if body.name == "Char1":
-		$Char1.moves = false;
+		$Char1.moves = false
 		await get_tree().create_timer(1.0).timeout
 		get_tree().change_scene_to_file("res://Scenes/Ordering/orderingScreen.tscn")
 
