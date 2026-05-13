e4bc3803ce96cda04b9425e95b9ab78886bfeefe
blueheart.elf <arislam.cs28@gmail.com>
2026-05-12 23:54:39 -0400

    Streamlined code to make sure that unnecessary lines weren't used to modify the speed

--- Characters/one_char.gd
+++ Characters/one_char.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 
 @export var speed: float = 200.0
 @export var direction: Vector2 = Vector2.LEFT 
-
+@export var moves = true
 
 func _physics_process(delta: float) -> void:
+	if !moves:
+		speed = 0.0
 	velocity = direction * speed
 	if speed > 0.0:
 		$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.stop()
 	move_and_slide()
 	
 
--- Scripts/ordering.gd
+++ Scripts/ordering.gd
@@ -14,8 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
 	if body.name == "Char1":
-		$Char1.speed = 0.0;
-		$Char1/AnimatedSprite2D.stop()
+		$Char1.moves = false;
 		await get_tree().create_timer(1.0).timeout
 		get_tree().change_scene_to_file("res://Scenes/Ordering/orderingScreen.tscn")
 
