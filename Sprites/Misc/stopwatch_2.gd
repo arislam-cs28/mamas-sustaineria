a1c38d7f227423797d237067faa786554368fd1e
blueheart.elf <arislam.cs28@gmail.com>
2026-05-18 17:51:07 -0400

    fixing things (?)

---
+++ Sprites/Misc/stopwatch_2.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+@onready var label = $Label3
+@onready var timer = $Timer
+
+func _ready():
+	await get_tree().create_timer(1.0).timeout
+	timer.start()
+	
+func time_left_still():
+	var time_left = timer.time_left
+	var minute = floor(time_left / 60)
+	var second = int(time_left) % 60
+	
+	return [minute, second]
+	
+func _process(delta):
+	label.text = "%02d:%02d" % time_left_still()
