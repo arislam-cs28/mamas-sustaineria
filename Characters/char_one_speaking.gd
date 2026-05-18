6bca68629ba110d7f3bced94decc83c0af6f1c3e
mabrennan-bit <mabrennan@ctemc.org>
2026-05-18 14:20:08 -0400

    Revert "Merge branch 'main' of https://github.com/arislam-cs28/mamas-sustaineria"
    This reverts commit 29460911ef67f5570d0bdb604624d503a59d7f07, reversing
    changes made to 47889d5d461b6c52093a2caef8c679d14232475b.

--- Characters/char_one_speaking.gd
+++ Characters/char_one_speaking.gd
@@ -5,7 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 
 	$AnimatedSprite2D.play()
 	await get_tree().create_timer(3.0).timeout
