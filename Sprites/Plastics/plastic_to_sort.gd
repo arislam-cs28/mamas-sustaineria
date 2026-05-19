7832f0b2a3727e371bd5b73a6d70ada16b9b1b1e
blueheart.elf <arislam.cs28@gmail.com>
2026-05-19 02:24:11 -0400

    i just made a random plastic sprite chooser but i need to reowrk this to fit with sorting logic

--- Sprites/Plastics/plastic_to_sort.gd
+++ Sprites/Plastics/plastic_to_sort.gd
@@ -1,12 +1,10 @@
 extends Sprite2D
 
 @export var plastics_available: Array[Texture2D]
+var plastic_name = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	texture = plastics_available.pick_random()
+	plastic_name = str(texture)
+	print(plastic_name)
