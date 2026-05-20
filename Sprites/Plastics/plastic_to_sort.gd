3c752dbd10101e0a7a6891f7f97a97323551625c
blueheart.elf <arislam.cs28@gmail.com>
2026-05-19 22:42:53 -0400

    wait i made the sprite draggable

--- Sprites/Plastics/plastic_to_sort.gd
+++ Sprites/Plastics/plastic_to_sort.gd
@@ -2,9 +2,29 @@ extends Sprite2D
 
 @export var plastics_available: Array[Texture2D]
 var plastic_name = ""
+var file_path
+var drags = false
+var delay = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	texture = plastics_available.pick_random()
-	plastic_name = str(texture)
+	file_path = texture.resource_path.get_file().get_basename()
+	plastic_name = file_path
 	print(plastic_name) 
+
+func _physics_process(delta: float) -> void:
+	if drags:
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", get_global_mouse_position(), delay * delta)
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			get_viewport().set_input_as_handled()
+			if get_rect().has_point(to_local(event.position)):
+				print("clicked pls work")
+				drags = true
+			else:
+				drags = false
+				print("it no work augh")
--- Sprites/Sorting/sorting.gd
+++ Sprites/Sorting/sorting.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print($/root/GameManage.what_player)
@@ -15,4 +14,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	pass # i did something wrong
